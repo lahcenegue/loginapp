cec38ecc0ff27623a4ce117ff360f0a9ce4ab002
lahcenegue <lahcene90.lg@gmail.com>
2023-01-11 08:09:29 +0100

    update register model

--- lib/screens/register/register_response_model.dart
+++ lib/screens/register/register_response_model.dart
@@ -1,3 +1,26 @@
+class RegisterRequestModel {
+  String? name;
+  String? email;
+  String? password;
+  String? civilNumer;
+  RegisterRequestModel({
+    this.name,
+    this.email,
+    this.password,
+    this.civilNumer,
+  });
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {
+      'name': name!.trim(),
+      'email': email!.trim(),
+      'password': password!.trim(),
+      'civilNumer': civilNumer!.trim(),
+    };
+
+    return map;
+  }
+}
+
 class RegisterResponseModel {
   String? msg;
   String? user;

a740a61858751c7bcd6fd0dcffe21b7819d1eeb8
lahcenegue <lahcene90.lg@gmail.com>
2023-01-18 14:07:44 -0800

    group model

---
+++ lib/screens/home_main/groupe/groupe_model.dart
@@ -0,0 +1,58 @@
+class GroupRequestModel {
+  String? name;
+  String? amountall;
+  String? amount;
+  String? comment;
+
+  GroupRequestModel({
+    this.name,
+    this.amountall,
+    this.amount,
+    this.comment,
+  });
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {
+      'name': name!.trim(),
+      'comment': comment!.trim(),
+      'amountall': amountall!.trim(),
+      'amout': amount!.trim(),
+    };
+
+    return map;
+  }
+}
+
+class GroupeResponseModel {
+  int? amount;
+  String? name;
+  String? amountall;
+  String? md5id;
+  String? comment;
+  String? show1;
+  String? date1;
+  int? sharebutton;
+
+  GroupeResponseModel({
+    this.amount,
+    this.name,
+    this.amountall,
+    this.md5id,
+    this.comment,
+    this.show1,
+    this.date1,
+    this.sharebutton,
+  });
+
+  factory GroupeResponseModel.fromJson(Map<String, dynamic> json) {
+    return GroupeResponseModel(
+      amount: json['amount'],
+      name: json['name'],
+      amountall: json['amountall'],
+      md5id: json['md5id'],
+      comment: json['comment'],
+      show1: json['show1'],
+      date1: json['date1'],
+      sharebutton: json['sharebutton'],
+    );
+  }
+}

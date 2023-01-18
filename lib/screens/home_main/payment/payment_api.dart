cff63ad804067151fe0bab31f9161f975bb8be3b
lahcenegue <lahcene90.lg@gmail.com>
2023-01-17 23:53:44 -0800

    load more

--- lib/screens/home_main/payment/payment_api.dart
+++ lib/screens/home_main/payment/payment_api.dart
@@ -3,6 +3,8 @@ import 'package:loginapp/screens/home_main/payment/payment_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+List<PaymentModel> payment = [];
+
 class PaymentApi {
   Future<List<PaymentModel>> loadData(
       {required String token, required int page}) async {
@@ -20,7 +22,10 @@ class PaymentApi {
         List<PaymentModel> paymentModel =
             list.map((e) => PaymentModel.fromJson(e)).toList();
 
-        return paymentModel;
+        payment = payment + paymentModel;
+        print("payment lenght: ${payment.length}");
+
+        return payment;
       }
     } catch (e) {
       throw Exception(e);

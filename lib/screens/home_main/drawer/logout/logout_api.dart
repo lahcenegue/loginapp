0776ae755733968a978ee12d7beb5ee91ed184de
lahcenegue <lahcene90.lg@gmail.com>
2023-01-27 08:17:18 +0100

    logout api

--- lib/screens/home_main/drawer/logout/logout_api.dart
+++ lib/screens/home_main/drawer/logout/logout_api.dart
@@ -1,1 +1,25 @@
+import 'package:loginapp/constants/constants.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+import 'package:loginapp/screens/home_main/drawer/logout/logout_model.dart';
+
+Future<LogoutModel> logoutApi({required String token}) async {
+  try {
+    var url = Uri.parse("${Constants.url}/api/logout?token=$token");
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var body = convert.jsonDecode(response.body);
+
+      LogoutModel logoutModel = LogoutModel.fromJson(body);
+
+      return logoutModel;
+    }
+  } catch (e) {
+    throw Exception(e);
+  }
+
+  return LogoutModel();
+}
